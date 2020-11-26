d2d49f0e90ffe70966df8725e1cb22f780106f4d
DUNNIK <dns.56@mail.ru>
2020-11-27 00:31:44 +0300

    Accidentally permanently deleted the Full Restore class. Wrote a new one

--- BackUpsLab/BackUp/RestorePoint/FullRestorePoint.cs
+++ BackUpsLab/BackUp/RestorePoint/FullRestorePoint.cs
@@ -1,7 +1,44 @@
-﻿namespace BackUpsLab.BackUp.RestorePoint
+﻿using System;
+using System.IO;
+using BackUpsLab.Exceptions;
+
+namespace BackUpsLab.BackUp.RestorePoint
 {
-    public class FullRestorePoint
+    public class FullRestorePoint : RestorePoint
     {
-        
+        public FullRestorePoint(BackUp backUp) : base(backUp)
+        {
+            
+        }
+
+        private void CreateRestoreForEveryFile()
+        {
+            var filePaths = BackUp.FilePaths;
+            foreach (var filePath in filePaths)
+            {
+                EveryFileInfo.Add(filePath, CreateRestore(filePath));
+            }
+        }
+        public override void CreateRestorePoint()
+        {
+            try
+            {
+                CreateRestoreForEveryFile();
+                BackUp.Manager.RestorePoints.Enqueue(this);
+            }
+            catch
+            {
+                throw new RestorePointException();
+            }
+        }
+
+        public override FileRestoreCopyInfo CreateRestore(string filePath)
+        {
+            var fileInfo = new FileInfo(filePath);
+            var fileRestoreCopyInfo = new FileRestoreCopyInfo
+                (filePath, fileInfo.Length, DateTime.Now);
+            Storage.AddFileTo(filePath);
+            return fileRestoreCopyInfo;
+        }
     }
 }
